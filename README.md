a296c996ee607e998526e5dec8b2412d1248d852
Luna <ciubix8514@gmail.com>
2023-03-14 14:24:27 +0300

    Added setup script

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 These are the config files I use
+
+
+
+The file setup.sh just puts all the config files in appropriate places, creating directories if necessary
---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp ./guake_pref ~/;
+cp ./.zshrc ~/;
+mkdir ~/.config/kitty;
+cp ./kitty.conf ~/.config/kitty/kitty.conf;
+mkdir ~/.config/lvim;
+cp ./lvim/config.lua ~/.config/lvim/;
